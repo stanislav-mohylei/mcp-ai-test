654672ce63970111b87dfdd5b126c6b017c68605
Stanislav Mohylei <122780457+stanislav-mohylei@users.noreply.github.com>
2026-03-09 15:32:20 +0200

    Create tests/check-client-base-v2.spec.ts

---
+++ tests/check-client-base-v2.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Check client base v2', () => {
+  test('should navigate to client work', async ({ page }) => {
+    /* TEST_STEPS_PLACEHOLDER */
+  });
+});
