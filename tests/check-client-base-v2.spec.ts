8588f5752dc0667d7b93e05f025ad5ab93494ea1
Stanislav Mohylei <122780457+stanislav-mohylei@users.noreply.github.com>
2026-03-10 12:50:25 +0200

    Create tests/check-client-base-v2.spec.ts

---
+++ tests/check-client-base-v2.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Check client base', () => {
+  test('navigate EPAM services and client work', async ({ page }) => {
+    // TEST_BODY
+  });
+});
